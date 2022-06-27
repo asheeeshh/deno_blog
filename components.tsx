4b35deec856e3d75d830cee1ecc86ffc8f4880ed
ashish <asheeshh@duck.com>
2022-06-27 13:57:09 +0530

    updates

--- components.tsx
+++ components.tsx
@@ -44,21 +44,21 @@ export function Index({ state, posts }: IndexProps) {
             {state.avatar && (
               <div
                 class={[
-                  "bg-cover bg-center bg-no-repeat w-25 h-25 border-4 border-white",
+                  "bg-cover bg-center bg-no-repeat w-25 h-25 border-4",
                   state.avatarClass ?? "rounded-full",
                 ].filter(Boolean).join(" ")}
                 style={{ backgroundImage: `url(${state.avatar})` }}
               />
             )}
             <h1
-              class="mt-3 text-4xl text-gray-900 font-bold"
+              class="mt-3 text-3xl text-gray-200"
               style={{ color: state.coverTextColor }}
             >
               {state.title ?? "My Blog"}
             </h1>
             {state.description && (
               <p
-                class="text-lg text-gray-600"
+                class="text-lg text-gray-300"
                 style={{ color: state.coverTextColor }}
               >
                 {state.description}
@@ -82,7 +82,7 @@ export function Index({ state, posts }: IndexProps) {
 
                   return (
                     <a
-                      class="relative flex items-center justify-center w-8 h-8 rounded-full bg-gray-600/10 text-gray-700 hover:bg-gray-600/15 hover:text-black transition-colors group"
+                      class="relative flex items-center justify-center w-8 h-8 rounded-full bg-gray-200/10 text-gray-500 hover:bg-gray-200/15 hover:text-black transition-colors group"
                       href={link.url}
                     >
                       {link.icon ? link.icon : <Icon />}
@@ -121,19 +121,19 @@ function PostCard({ post, timezone }: { post: Post; timezone: string }) {
           {post.title}
         </a>
       </h3>
-      <p class="text-gray-500/80">
+      <p class="text-gray-200/80">
         <PrettyDate date={post.publishDate} timezone={timezone} />
       </p>
-      <p class="mt-3 text-gray-600">
+      <p class="mt-3 text-gray-300">
         {post.snippet}
       </p>
       <p class="mt-3">
         <a
-          class="leading-tight text-gray-900 inline-block border-b-1 border-gray-600 hover:text-gray-500 hover:border-gray-500 transition-colors"
+          class="leading-tight text-gray-300 inline-block border-b-1 border-gray-300 hover:text-gray-200 hover:border-gray-200 transition-colors"
           href={post.pathname}
           title={`Read "${post.title}"`}
         >
-          Read More
+          read more
         </a>
       </p>
     </div>
@@ -152,20 +152,10 @@ export function PostPage({ post, state }: PostPageProps) {
       <div class="pb-16">
         <a
           href="/"
-          class="inline-flex items-center gap-1 text-sm text-gray-500/80 hover:text-gray-700 transition-colors"
+          class="inline-flex items-center gap-1 text-sm text-gray-200/80 hover:text-gray-300 transition-colors"
           title="Back to Index Page"
         >
-          <svg
-            className="inline-block w-5 h-5"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M6.91675 14.4167L3.08341 10.5833C3.00008 10.5 2.94119 10.4097 2.90675 10.3125C2.87175 10.2153 2.85425 10.1111 2.85425 10C2.85425 9.88889 2.87175 9.78472 2.90675 9.6875C2.94119 9.59028 3.00008 9.5 3.08341 9.41667L6.93758 5.5625C7.09036 5.40972 7.27786 5.33334 7.50008 5.33334C7.7223 5.33334 7.91675 5.41667 8.08341 5.58334C8.23619 5.73611 8.31258 5.93056 8.31258 6.16667C8.31258 6.40278 8.23619 6.59722 8.08341 6.75L5.66675 9.16667H16.6667C16.9029 9.16667 17.1006 9.24639 17.2601 9.40584C17.4201 9.56584 17.5001 9.76389 17.5001 10C17.5001 10.2361 17.4201 10.4339 17.2601 10.5933C17.1006 10.7533 16.9029 10.8333 16.6667 10.8333H5.66675L8.10425 13.2708C8.25703 13.4236 8.33341 13.6111 8.33341 13.8333C8.33341 14.0556 8.25008 14.25 8.08341 14.4167C7.93064 14.5694 7.73619 14.6458 7.50008 14.6458C7.26397 14.6458 7.06953 14.5694 6.91675 14.4167Z"
-              fill="currentColor"
-            />
-          </svg>
-          INDEX
+          ../index
         </a>
       </div>
       {post.coverHtml && (
@@ -175,13 +165,13 @@ export function PostPage({ post, state }: PostPageProps) {
         />
       )}
       <article>
-        <h1 class="text-4xl text-gray-900 font-bold">
+        <h1 class="text-4xl text-gray-300 font-bold">
           {post.title}
         </h1>
-        <p class="mt-1 text-gray-500">
+        <p class="mt-1 text-gray-300">
           {(state.author || post.author) && (
             <span>
-              By {state.author || post.author} at {" "}
+              by {state.author || post.author} at {" "}
             </span>
           )}
           <PrettyDate date={post.publishDate} timezone={state.timezone} />
@@ -202,19 +192,19 @@ export function PostPage({ post, state }: PostPageProps) {
 function Footer(props: { author?: string }) {
   return (
     <footer class="mt-20 pb-16 lt-sm:pb-8 lt-sm:mt-16">
-      <p class="flex items-center gap-2.5 text-gray-400/800 text-sm">
+      <p class="flex items-center gap-2.5 text-gray-300 text-sm">
         <span>
-          &copy; {new Date().getFullYear()} {props.author}, Powered by{" "}
+          &copy; {new Date().getFullYear()} {props.author}, powered by{" "}
           <a
             class="inline-flex items-center gap-1 underline hover:text-gray-800 transition-colors"
             href="https://deno.land/x/blog"
           >
-            Deno Blog
+            deno blog
           </a>
         </span>
         <a
           href="/feed"
-          class="inline-flex items-center gap-1 hover:text-gray-800 transition-colors"
+          class="inline-flex items-center gap-1 hover:text-gray-200 transition-colors"
           title="Atom Feed"
         >
           <IconRssFeed /> RSS
